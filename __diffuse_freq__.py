ad265f121ab1c026d5e0234278786abb76ae1e80
EpicWings <pasca_robert2003@yahoo.com>
2023-06-29 15:11:53 +0300

    Working on decrypt

--- __diffuse_freq__.py
+++ __diffuse_freq__.py
@@ -5,39 +5,69 @@ import matplotlib.pyplot as plt
 import sympy
 from scipy.io import wavfile
 
-# define
-N = 4
 
 
 def load_data(filename):
     sample_rate, audio_data = wavfile.read(filename)
+    duration = len(audio_data)/sample_rate
 
-    return sample_rate, audio_data
+    data = [duration, sample_rate, audio_data]
 
+    return data
 
-def encrypt(sample_rate, audio_data, output_file):
+def encrypt(data, output_file):
+
+    # Get data
+    duration = data[0]
+    sample_rate = data[1]
+    audio_data = data[2]
+
+    N = 10
     
     spectrum = scipy.fft.fft(audio_data)
-    spectrum = numpy.array(spectrum, dtype=numpy.complex64)
-    spectrum = numpy.real(spectrum)
 
-    plt.plot(spectrum)
-    plt.plot(audio_data)
-    plt.show()
+    sub_band = numpy.split(spectrum, N)
+    sub_band_idx = numpy.random.permutation(N)
 
-    # write to file
-    wavfile.write(output_file, sample_rate, spectrum)
+    sub_band_shuffled = [sub_band[i] for i in sub_band_idx]
+
+    spectrum_shuffled = numpy.concatenate(sub_band_shuffled)
+
+    encrypted_spectrum = scipy.fft.ifft(spectrum_shuffled)
+
+    enc_audio_data = numpy.real(encrypted_spectrum)
+
+    enc_audio_data = enc_audio_data.astype(numpy.int16)
+
+    wavfile.write(output_file, sample_rate, enc_audio_data)
+    
+    return sub_band_idx
+
+def decrypt(input_file, output_file, key):
+    
+    # Get data
+    sample_rate, enc_audio_data = wavfile.read(input_file)
+    sub_band_idx = key
+
+    print(sub_band_idx)
 
 
-def decrypt(input_file, output_file):
-    # read from file
-    sample_rate, audio_data = load_data(input_file)
+    spectrum_shuffled = scipy.fft.fft(enc_audio_data)
 
-    # decrypt
-    spectrum = numpy.array(audio_data, dtype=numpy.complex64)
-    spectrum = scipy.fft.ifft(spectrum)
-    spectrum = numpy.real(spectrum)
-    spectrum = numpy.array(spectrum, dtype=numpy.int16)
+    N = 10
 
-    # write to file
-    wavfile.write(output_file, sample_rate, spectrum)
+    sub_band_shuffled = numpy.split(spectrum_shuffled, N)
+
+    sub_band = [sub_band_shuffled[sub_band_idx[i]] for i in range(N)]
+
+    spectrum = numpy.concatenate(sub_band)
+
+    decrypted_spectrum = scipy.fft.ifft(spectrum)
+
+    audio_data = numpy.real(decrypted_spectrum)
+
+    audio_data = audio_data.astype(numpy.int16)
+
+    decrypted_data = [len(audio_data), sample_rate, audio_data]
+
+    wavfile.write(output_file, sample_rate, audio_data)
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def main():
     filename = "CantinaBand60.wav"
     output_file = "CantinaBand60_encrypted.wav"
 
-    sample_rate, audio_data = __diffuse_freq__.load_data(filename)
+    data = __diffuse_freq__.load_data(filename)
 
-    __diffuse_freq__.encrypt(sample_rate, audio_data, output_file)
+    key = __diffuse_freq__.encrypt(data, output_file)
 
-    __diffuse_freq__.decrypt(output_file, "CantinaBand60_decrypted.wav")
+    __diffuse_freq__.decrypt(output_file, "CantinaBand60_decrypted.wav", key)
 
 if __name__ == "__main__":
     main()
